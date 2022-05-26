036be142d4ed830c092a72beb13c020715516eff
chan <cham9994@gmail.com>
2022-05-26 18:52:56 +0900

    API 호출

--- js/main.js
+++ js/main.js
@@ -1,8 +1,54 @@
-console.log("Hello Parcel!!")
+// 모듈 import
+import axios from 'axios'
 
-async function test() {
-  const promise = Promise.resolve(123)
-  console.log(await promise)
+// API 정보
+const API_URL = 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/todos'
+const API_KEY = 'FcKdtJs202204'
+const USER_NAME = 'GyeongChan'
+
+// 할 일 등록하기
+async function createTodo() {
+  const res = await axios({
+    url: API_URL,
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json',
+      'apikey': API_KEY,
+      'username': 'GyeongChan'
+    },
+    data: {
+      'title': '처음으로 포스트를 날려봅니다!'
+    }
+  })
+  console.log(res)
 }
 
-test()
+// createTodo()
+
+// 할 일 가져오기
+async function getTodo() {
+  const res = await axios({
+    url: API_URL,
+    method: 'GET',
+    headers: {
+      "content-type": "application/json",
+      "apikey": API_KEY,
+      'username': USER_NAME
+    }
+  })
+  return res
+}
+
+// 할 일 삭제하기
+async function deleteTodo(id) {
+  const res = await axios({
+    url: `${API_URL}/${id}`,
+    method: 'DELETE',
+    headers: {
+      "content-type": "application/json",
+      "apikey": API_KEY,
+      'username': USER_NAME
+    }
+  })
+  return res
+}
